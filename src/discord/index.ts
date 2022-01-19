1853d312f74023e5aa2516a7b82dadf927376195
Artemis Robbins <97853037+ThatSoloTaco@users.noreply.github.com>
2022-01-18 21:38:12 -0500

    Delete messages on time delay (#19)
    * Adding a feature to delete messages on a certain channel after X time in
    ms
    
    * Adding additional intent for guild messages
    
    * Fixing typo on time_ms
    
    * Fixing parse issue

--- src/discord/index.ts
+++ src/discord/index.ts
@@ -2,14 +2,17 @@ import { Client, Intents } from 'discord.js';
 
 import config from '../config';
 import { handleInteraction } from './interactionRouter';
+import { deleteMessageOnDelay } from './listeners/deleteMessageOnDelay';
 
-export const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+export const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 export const initializeDiscord = (callback?: () => void) => {
   client.once('ready', () => {
     console.log('Started OSRS Leagues Bot!');
     callback?.();
   });
+  
+  client.on('messageCreate', deleteMessageOnDelay);
 
   client.on('interactionCreate', handleInteraction);
 
