f7c948e62ea047e7c8c8f2cee23e47c24bda4551
Allen Kinzalow <allen.kinzalow@gmail.com>
2022-01-18 20:16:50 -0500

    Add remove roles command. (#23)
    * Add remove roles command.
    
    * Update workflow to tag upon release.

---
+++ src/discord/commands/remove_roles.ts
@@ -0,0 +1,43 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { GuildMember } from 'discord.js';
+
+import { DiscordUser } from '../../database/models';
+import removeLeagueRoles from '../actions/removeLeagueRoles';
+import { Command } from './types';
+
+const channels = [
+  /**
+   * OSRS Leagues server
+   */
+  '769283619595485224', // #bot-commands
+  '636193036195463178', // #bot-commands-test
+
+  /**
+   * Bot Testing Server
+   */
+  '931963036896464946', // #bot-commands
+];
+
+const removeRolesCommand: Command = {
+  channels,
+  data: new SlashCommandBuilder()
+    .setName('remove_roles')
+    .setDescription('Remove all of your league roles'),
+  execute: async (interaction) => {
+    const discordMember = interaction.member;
+    const result = await DiscordUser.findByPk(discordMember.user.id);
+    if (result) {
+      await result.update({
+        twisted_name: null,
+        trailblazer_name: null,
+        shattered_relics_name: null,
+      });
+    }
+    await removeLeagueRoles({
+      member: discordMember as GuildMember,
+    });
+    await interaction.reply('Removed all of your league roles!');
+  },
+};
+
+export default removeRolesCommand;
