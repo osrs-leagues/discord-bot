9c68cd3631eb9ce2bd9e52f8eb1326bbcb4ab90f
Allen Kinzalow <allen.kinzalow@gmail.com>
2022-01-19 19:02:50 -0500

    Fix puppeteer on Heroku (#29)
    * Fix puppeteer for heroku & fix the leaguePointRankings job.
    
    * Changeset

---
+++ .changeset/itchy-windows-joke.md
@@ -0,0 +1,5 @@
+---
+'@osrs-leagues/discord-bot': patch
+---
+
+Fix puppeteer on heroku and fix the league point rankings job.
--- src/schedule/jobs/leaguePointRankings.ts
+++ src/schedule/jobs/leaguePointRankings.ts
@@ -8,18 +8,21 @@ const leaguePointRankingsJob: Job = {
   interval: {
     testing: undefined,
     development: undefined, //'* * * * *',
-    stage: '* * * *',
-    production: '* * * *',
+    stage: '* * * * *',
+    production: '0 */1 * * *',
   },
   schedule: () => {
-    const interval = leaguePointRankingsJob.interval;
-    cron.schedule(interval[config.environment], () => {
-      try {
-        fetchLeaguePointRankings.execute();
-      } catch (error) {
-        console.error('Error executing leaguePointRankingsJob.', error);
-      }
-    });
+    const intervalMap = leaguePointRankingsJob.interval;
+    const interval = intervalMap[config.environment];
+    if (interval) {
+      cron.schedule(interval, () => {
+        try {
+          fetchLeaguePointRankings.execute();
+        } catch (error) {
+          console.error('Error executing leaguePointRankingsJob.', error);
+        }
+      });
+    }
   },
 };
 
