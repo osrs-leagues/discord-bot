2fa1c2851293081d654c8d7fc1722050b8e0ad1f
Allen Kinzalow <allen.kinzalow@gmail.com>
2024-11-13 23:00:00 -0500

    [#89] Add challenge_statistics command

--- src/discord/interactions/commands/index.ts
+++ src/discord/interactions/commands/index.ts
@@ -24,6 +24,7 @@ import raffleDrawCommand from './draw_raffle';
 import leagueNameRemote from './leagueNameRemote';
 import approveChallengeCommand from './approveChallenge';
 import rejectChallengeCommand from './rejectChallenge';
+import challengeStatisticsCommand from './challengeStatistics';
 
 const commandData = [
   pingCommand,
@@ -47,6 +48,7 @@ const commandData = [
   raffleDrawCommand,
   approveChallengeCommand,
   rejectChallengeCommand,
+  challengeStatisticsCommand,
 ];
 
 const commands = new Collection<string, Command>();
