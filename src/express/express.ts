549877ec68e4313db8427555313edb78e93fad66
Allen Kinzalow <allen.kinzalow@gmail.com>
2024-11-17 15:25:42 -0500

    dependencies and structure

---
+++ src/express/express.ts
@@ -0,0 +1,24 @@
+import bodyParser from 'body-parser';
+import compress from 'compression';
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+
+const app = express();
+
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors());
+
+// gzip compression
+app.use(compress());
+
+// secure apps by setting various HTTP headers
+app.use(helmet());
+
+// parse body params and attache them to req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api', apis);
+
+export default app;
