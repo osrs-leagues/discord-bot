2315cbc0518e3404e18d7586319be40428ee8ace
Allen Kinzalow <allen.kinzalow@gmail.com>
2024-11-25 18:01:24 -0500

    fix tests

--- src/database/models/__tests__/ChallengeCard.test.ts
+++ src/database/models/__tests__/ChallengeCard.test.ts
@@ -1,32 +1,50 @@
+import { challengeCache } from '../../../challenges';
+import { ChallengeDifficulty } from '../Challenge';
 import ChallengeCard, { ChallengeCardStatus } from '../Challenge/ChallengeCard';
 
 describe('ChallengeCard', () => {
   test('getChallengeIds should filter out undefined challenges', async () => {
+    const noviceChallenges = challengeCache.challenges.filter(
+      (challenge) => challenge.difficulty === ChallengeDifficulty.NOVICE,
+    );
     const challengeCard = await ChallengeCard.create({
-      challengeOneId: 1,
-      challengeTwoId: 2,
-      challengeThreeId: 3,
+      challengeOneId: noviceChallenges[0]?.id,
+      challengeTwoId: noviceChallenges[1]?.id,
+      challengeThreeId: noviceChallenges[2]?.id,
       challengeFiveId: undefined,
-      difficulty: 1,
+      difficulty: ChallengeDifficulty.NOVICE,
       discordUserId: '1234',
       rerollsRemaining: 2,
       status: ChallengeCardStatus.STARTED,
     });
-    expect(challengeCard.getChallengeIds()).toEqual([1, 2, 3]);
+    expect(challengeCard.getChallengeIds()).toEqual([
+      noviceChallenges[0]?.id,
+      noviceChallenges[1]?.id,
+      noviceChallenges[2]?.id,
+    ]);
   });
 
   test('getChallengeIds should return ids for all challenge foreign keys', async () => {
+    const masterChallenges = challengeCache.challenges.filter(
+      (challenge) => challenge.difficulty === ChallengeDifficulty.MASTER,
+    );
     const challengeCard = await ChallengeCard.create({
-      challengeOneId: 1,
-      challengeTwoId: 2,
-      challengeThreeId: 3,
-      challengeFourId: 4,
-      challengeFiveId: 5,
-      difficulty: 3,
+      challengeOneId: masterChallenges[0]?.id,
+      challengeTwoId: masterChallenges[1]?.id,
+      challengeThreeId: masterChallenges[2]?.id,
+      challengeFourId: masterChallenges[3]?.id,
+      challengeFiveId: masterChallenges[4]?.id,
+      difficulty: ChallengeDifficulty.MASTER,
       discordUserId: '1234',
       rerollsRemaining: 2,
       status: ChallengeCardStatus.STARTED,
     });
-    expect(challengeCard.getChallengeIds()).toEqual([1, 2, 3, 4, 5]);
+    expect(challengeCard.getChallengeIds()).toEqual([
+      masterChallenges[0]?.id,
+      masterChallenges[1]?.id,
+      masterChallenges[2]?.id,
+      masterChallenges[3]?.id,
+      masterChallenges[4]?.id,
+    ]);
   });
 });
