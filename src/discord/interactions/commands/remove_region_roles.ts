79a4ff61d9b529d1691311100d7d51d43c8f4e81
Allen Kinzalow <allen.kinzalow@gmail.com>
2024-11-25 19:51:00 -0500

    [#111] Fix remove region role command (#112)
    * [#111[ Fix remove region role command
    
    * typos

---
+++ src/discord/interactions/commands/remove_region_roles.ts
@@ -0,0 +1,35 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { GuildMember } from 'discord.js';
+
+import { Command } from './types';
+import { channelGroups } from '../../Channel';
+import { setRegionRoles } from '../../actions';
+
+const removeRegionRolesCommand: Command = {
+  channels: channelGroups.BOT_COMMANDS,
+  data: new SlashCommandBuilder()
+    .setName('remove_region_roles')
+    .setDescription('Remove you region role(s).'),
+  execute: async (interaction) => {
+    try {
+      await setRegionRoles({
+        guild: interaction.guild,
+        member: interaction.member as GuildMember,
+        regions: [],
+      });
+      interaction.reply({
+        content: 'Your region role(s) have been removed.',
+        ephemeral: true,
+      });
+    } catch (error) {
+      console.error(`Error responding to remove region role command: ${error}`);
+      interaction.reply({
+        content:
+          'There was a problem removing your region role(s). Please try again.',
+        ephemeral: true,
+      });
+    }
+  },
+};
+
+export default removeRegionRolesCommand;
