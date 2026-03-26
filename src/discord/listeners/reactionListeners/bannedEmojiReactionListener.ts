4252e0e542ce2b66148a2513fd4e839a6100af00
Hammer-God <gimhammerx@gmail.com>
2026-03-26 23:10:24 +0000

    Improved banned emoji check to automatically check for all skintone variants of an emoji without needing to insert all into an array.

--- src/discord/listeners/reactionListeners/bannedEmojiReactionListener.ts
+++ src/discord/listeners/reactionListeners/bannedEmojiReactionListener.ts
@@ -1,13 +1,13 @@
 import { MessageReaction } from 'discord.js';
 import { ReactionListener } from '../types';
 
-const BANNED_EMOJIS = ['🫃', '🫃🏻', '🫃🏼', '🫃🏽', '🫃🏾', '🫃🏿'];
+const BANNED_EMOJIS = ['🫃'];
 
 const bannedEmojiReactionListener: ReactionListener = {
   onMessageReaction: (reaction: MessageReaction) => {
     try {
       const emoji = reaction.emoji.name;
-      if (BANNED_EMOJIS.includes(emoji)) {
+      if (BANNED_EMOJIS.some((e) => emoji.startsWith(e))) {
         reaction.remove().catch((err) => {
           if (err.code === 10014) return;
           console.error('Error removing banned emoji reaction:', err);
